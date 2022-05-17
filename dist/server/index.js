d378a37b5aca3600869d3e650f06493e8fd847c2
Glarell <pilou.tondon@hotmail.fr>
2022-05-17 13:06:27 +0200

    clean up

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,40 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import IndexComponent from "./Component/IndexComponent";
-import http from "http";
-import * as WebSocket from 'ws';
-import express from 'express';
-
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-
-root.render(
-    <IndexComponent/>
-);
-
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = require("react");
+const client_1 = require("react-dom/client");
+const IndexComponent_1 = require("./Component/IndexComponent");
+const http_1 = require("http");
+const WebSocket = require("ws");
+const express_1 = require("express");
+const root = client_1.default.createRoot(document.getElementById('root'));
+root.render(<IndexComponent_1.default />);
 class Server {
-
-    MESSAGE(subscription:string,id:number,queue:string) {
+    MESSAGE(subscription, id, queue) {
         return `MESSAGE
 subscription:${subscription}
 message-id:${id}
 destination:/queue/${queue}
 content-type:text/plain
 
-hello queue a^@`
+hello queue a^@`;
     }
-
-    RECEIPT(id:number) {
+    RECEIPT(id) {
         return `RECEIPT
 receipt-id:message-${id}
 
-^@`
+^@`;
     }
-
-    ERROR(id:number,queue:string,content_length:number) {
+    ERROR(id, queue, content_length) {
         return `ERROR
 receipt-id:message-${id}
 content-type:text/plain
@@ -51,36 +41,29 @@ Hello queue ${queue}!
 -----
 Did not contain a destination header, which is REQUIRED
 for message propagation.
-^@`
+^@`;
     }
 }
-const app = express();
-
+const app = (0, express_1.default)();
 //initialize a simple http server
-const server = http.createServer(app);
-
-app.get("/", (req: any, res: any) => {
+const server = http_1.default.createServer(app);
+app.get("/", (req, res) => {
     res.send("hello world");
 });
-
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws: WebSocket) => {
-
+wss.on('connection', (ws) => {
     //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
+    ws.on('message', (message) => {
         //log the received message and send it back to the client
         console.log('received: %s', message);
         ws.send(`Hello, you sent -> ${message}`);
     });
-
     //send immediatly a feedback to the incoming connection
     ws.send('Hi there, I am a WebSocket server');
 });
-
 //start our server
 server.listen(8080, () => {
     console.log(`Server started on port ${8080} :)`);
 });
+//# sourceMappingURL=index.js.map
